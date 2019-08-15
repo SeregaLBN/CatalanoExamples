60de52857cbeb7315f4cb37cd6fcb1760592d382
SeregaLBN <SeregaLBN@gmail.com>
2019-08-15 11:41:13 +0300

    first unit test

---
+++ src/test/java/ksn/imgusage/utils/UnitTest.java
@@ -0,0 +1,70 @@
+package ksn.imgusage.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UnitTest {
+
+    @BeforeAll
+    static void initLogger() {
+        System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY  , "TRACE");
+        System.setProperty(org.slf4j.impl.SimpleLogger.SHOW_DATE_TIME_KEY     , "true");
+        System.setProperty(org.slf4j.impl.SimpleLogger.DATE_TIME_FORMAT_KEY   , "HH:mm:ss:SSS");
+        System.setProperty(org.slf4j.impl.SimpleLogger.SHOW_THREAD_NAME_KEY   , "true");
+        System.setProperty(org.slf4j.impl.SimpleLogger.SHOW_LOG_NAME_KEY      , "true");
+        System.setProperty(org.slf4j.impl.SimpleLogger.SHOW_SHORT_LOG_NAME_KEY, "true");
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(UnitTest.class);
+
+    @Test
+    public void relativePathTest() {
+        File path = new File("/var/data/stuff/xyz.dat");
+        File base = new File("/var/data");
+        File relative = SelectFilterDialog.getRelativePath(path, base);
+        logger.info("relative={}", relative);
+        assertEquals("stuff/xyz.dat", relative.toString());
+
+
+        path = new File("/proj/java/ImageFilterExamples/exampleImages/image.jpg");
+        base = new File("/proj/java/ImageFilterExamples/exampleImages/image.pipeline.json").getParentFile();
+        relative = SelectFilterDialog.getRelativePath(path, base);
+        logger.info("relative={}", relative);
+        assertEquals("image.jpg", relative.toString());
+
+
+        path = new File("./exampleImages/image.jpg");
+        base = new File("./exampleImages/image.pipeline.json").getParentFile();
+        relative = SelectFilterDialog.getRelativePath(path, base);
+        logger.info("relative={}", relative);
+        assertEquals("image.jpg", relative.toString());
+
+
+        path = new File("/path/dir1/image.jpg");
+        base = new File("/path/dir2/image.pipeline.json").getParentFile();
+        relative = SelectFilterDialog.getRelativePath(path, base);
+        logger.info("relative={}", relative);
+        assertEquals("../dir1/image.jpg", relative.toString());
+
+
+        path = new File("path/dir1/image.jpg");
+        base = new File("path/dir2/image.pipeline.json").getParentFile();
+        relative = SelectFilterDialog.getRelativePath(path, base);
+        logger.info("relative={}", relative);
+        assertEquals("../dir1/image.jpg", relative.toString());
+
+
+        path = new File("dir1/image.jpg");
+        base = new File("dir2/image.pipeline.json").getParentFile();
+        relative = SelectFilterDialog.getRelativePath(path, base);
+        logger.info("relative={}", relative);
+        assertEquals("../dir1/image.jpg", relative.toString());
+    }
+
+}
